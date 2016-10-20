d711e9e514a7fbd6fca777230bc3bdd33febd4b5
dylan <dylan.ross@careotter.com>
2016-10-20 17:13:18 -0500

    Added nuspec and packaged nupkgs

---
+++ package.ps1
@@ -0,0 +1,2 @@
+﻿.\nuget.exe pack .\CareOtter.ServiceFabricUtilities\CareOtter.ServiceFabricUtilities.csproj -Properties "Platform=x64;Configuration=Release" -Sym -Build -BasePath .\CareOtter.ServiceFabricUtilities
+.\nuget.exe pack .\CareOtter.ServiceFabricUtilities.Mocks\CareOtter.ServiceFabricUtilities.Mocks.csproj -Properties "Platform=x64;Configuration=Release" -Sym -Build -BasePath .\CareOtter.ServiceFabricUtilities.Mocks
